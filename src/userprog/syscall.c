c6aae92d311e6b3c77d30241f2608dda887735a5
yanglinshu <nicolasvandarkholme@outlook.com>
2023-08-26 22:11:56 +0800

    refactor(src/userprog/syscall.c): remove dependency on esp

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -17,24 +17,24 @@
 
 static void syscall_handler (struct intr_frame *);
 
-static void syscall_halt (struct intr_frame* fr);
-static void syscall_exit (struct intr_frame* fr, int status);
-static tid_t syscall_exec (struct intr_frame* fr, const char *cmd_line);
-static int syscall_wait (struct intr_frame* fr, tid_t pid);
-static bool syscall_create (struct intr_frame* fr, const char *file, off_t initial_size);
-static bool syscall_remove (struct intr_frame* fr, const char *file);
-static int syscall_open (struct intr_frame* fr, const char *file);
-static int syscall_filesize (struct intr_frame* fr, int fd);
-static int syscall_read (struct intr_frame* fr, int fd, void *buffer, unsigned size);
-static int syscall_write (struct intr_frame* fr, int fd, const void *buffer, unsigned size);
-static void syscall_seek (struct intr_frame* fr, int fd, unsigned position);
-static unsigned syscall_tell (struct intr_frame* fr, int fd);
-static void syscall_close (struct intr_frame* fr, int fd);
+static void syscall_halt (void);
+static void syscall_exit (int status);
+static tid_t syscall_exec (const char *cmd_line);
+static int syscall_wait (tid_t pid);
+static bool syscall_create (const char *file, off_t initial_size);
+static bool syscall_remove (const char *file);
+static int syscall_open (const char *file);
+static int syscall_filesize (int fd);
+static int syscall_read (int fd, void *buffer, unsigned size);
+static int syscall_write (int fd, const void *buffer, unsigned size);
+static void syscall_seek (int fd, unsigned position);
+static unsigned syscall_tell (int fd);
+static void syscall_close (int fd);
 
-static bool is_valid_vaddr (const void* esp, const void *vaddr, bool write);
-static bool is_valid_vrange (const void* esp, const void *vaddr, unsigned size, bool write);
-static bool is_valid_word (const void* esp, const void *vaddr, bool write);
-static bool is_valid_string (const void* esp, const char *str, bool write);
+static bool is_valid_vaddr (const void *vaddr, bool write);
+static bool is_valid_vrange (const void *vaddr, unsigned size, bool write);
+static bool is_valid_word (const void *vaddr, bool write);
+static bool is_valid_string (const char *str, bool write);
 
 void
 syscall_init (void) 
@@ -45,8 +45,8 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f) 
 {
-  if (!is_valid_word(f->esp, f->esp, false)) {
-    syscall_exit(f, -1);
+  if (!is_valid_word(f->esp, false)) {
+    syscall_exit(-1);
   }
 
   int sys_code = *(int *)f->esp;
@@ -54,113 +54,113 @@ syscall_handler (struct intr_frame *f)
   switch (sys_code)
     {
       case SYS_HALT:
-        syscall_halt (f);
+        syscall_halt ();
         break;
       case SYS_EXIT:
-        if (!is_valid_word(f->esp, f->esp + 4, false))       
-          syscall_exit(f, -1);      
-        syscall_exit(f, *(int *)(f->esp + 4));
+        if (!is_valid_word(f->esp + 4, false))       
+          syscall_exit(-1);      
+        syscall_exit(*(int *)(f->esp + 4));
         break;
       case SYS_EXEC:
-        if (!is_valid_word(f->esp, f->esp + 4, false))
-          syscall_exit(f, -1);        
-        f->eax = syscall_exec(f, *(char **)(f->esp + 4));
+        if (!is_valid_word(f->esp + 4, false))
+          syscall_exit(-1);        
+        f->eax = syscall_exec(*(char **)(f->esp + 4));
         break;
       case SYS_WAIT:
-        if (!is_valid_word(f->esp, f->esp + 4, false))
-          syscall_exit(f, -1);        
-        f->eax = syscall_wait(f, *(tid_t *)(f->esp + 4));
+        if (!is_valid_word(f->esp + 4, false))
+          syscall_exit(-1);        
+        f->eax = syscall_wait(*(tid_t *)(f->esp + 4));
         break;
       case SYS_CREATE:
-        if (!is_valid_word(f->esp, f->esp + 4, false) || 
-            !is_valid_word(f->esp, f->esp + 8, false)) 
-          syscall_exit(f, -1);        
-        f->eax = syscall_create(f, *(char **)(f->esp + 4), *(off_t *)(f->esp + 8));
+        if (!is_valid_word(f->esp + 4, false) || 
+            !is_valid_word(f->esp + 8, false)) 
+          syscall_exit(-1);        
+        f->eax = syscall_create(*(char **)(f->esp + 4), *(off_t *)(f->esp + 8));
         break;
       case SYS_REMOVE:
-        if (!is_valid_word(f->esp, f->esp + 4, false))
-          syscall_exit(f, -1);        
-        f->eax = syscall_remove(f, *(char **)(f->esp + 4));
+        if (!is_valid_word(f->esp + 4, false))
+          syscall_exit(-1);        
+        f->eax = syscall_remove(*(char **)(f->esp + 4));
         break;
       case SYS_OPEN:
-        if (!is_valid_word(f->esp, f->esp + 4, false))
-          syscall_exit(f, -1);        
-        f->eax = syscall_open(f, *(char **)(f->esp + 4));
+        if (!is_valid_word(f->esp + 4, false))
+          syscall_exit(-1);        
+        f->eax = syscall_open(*(char **)(f->esp + 4));
         break;
       case SYS_FILESIZE:
-        if (!is_valid_word(f->esp, f->esp + 4, false))
-          syscall_exit(f, -1);        
-        f->eax = syscall_filesize(f, *(int *)(f->esp + 4));
+        if (!is_valid_word(f->esp + 4, false))
+          syscall_exit(-1);        
+        f->eax = syscall_filesize(*(int *)(f->esp + 4));
         break;
       case SYS_READ:
-        if (!is_valid_word(f->esp, f->esp + 4, false) || 
-            !is_valid_word(f->esp, f->esp + 8, false) || 
-            !is_valid_word(f->esp, f->esp + 12, false)) 
-          syscall_exit(f, -1);        
-        f->eax = syscall_read(f, *(int *)(f->esp + 4), *(void **)(f->esp + 8),
+        if (!is_valid_word(f->esp + 4, false) || 
+            !is_valid_word(f->esp + 8, false) || 
+            !is_valid_word(f->esp + 12, false)) 
+          syscall_exit(-1);        
+        f->eax = syscall_read(*(int *)(f->esp + 4), *(void **)(f->esp + 8),
                     *(unsigned *)(f->esp + 12));
         break;
       case SYS_WRITE:
-        if (!is_valid_word(f->esp, f->esp + 4, false) || 
-            !is_valid_word(f->esp, f->esp + 8, false) || 
-            !is_valid_word(f->esp, f->esp + 12, false)) 
-          syscall_exit(f, -1);        
-        f->eax = syscall_write(f, *(int *)(f->esp + 4), *(void **)(f->esp + 8),
+        if (!is_valid_word(f->esp + 4, false) || 
+            !is_valid_word(f->esp + 8, false) || 
+            !is_valid_word(f->esp + 12, false)) 
+          syscall_exit(-1);        
+        f->eax = syscall_write(*(int *)(f->esp + 4), *(void **)(f->esp + 8),
                     *(unsigned *)(f->esp + 12));
         break;
       case SYS_SEEK:
-        if (!is_valid_word(f->esp, f->esp + 4, false) || 
-            !is_valid_word(f->esp, f->esp + 8, false)) 
-          syscall_exit(f, -1);        
-        syscall_seek(f, *(int *)(f->esp + 4), *(unsigned *)(f->esp + 8));
+        if (!is_valid_word(f->esp + 4, false) || 
+            !is_valid_word(f->esp + 8, false)) 
+          syscall_exit(-1);        
+        syscall_seek(*(int *)(f->esp + 4), *(unsigned *)(f->esp + 8));
         break;
       case SYS_TELL:
-        if (!is_valid_word(f->esp, f->esp + 4, false))
-          syscall_exit(f, -1);        
-        f->eax = syscall_tell(f, *(int *)(f->esp + 4));
+        if (!is_valid_word(f->esp + 4, false))
+          syscall_exit(-1);        
+        f->eax = syscall_tell(*(int *)(f->esp + 4));
         break;
       case SYS_CLOSE:
-        if (!is_valid_word(f->esp, f->esp + 4, false))
-          syscall_exit(f, -1);        
-        syscall_close(f, *(int *)(f->esp + 4));
+        if (!is_valid_word(f->esp + 4, false))
+          syscall_exit(-1);        
+        syscall_close(*(int *)(f->esp + 4));
         break;
       default:
-        syscall_exit(f, -1);
+        syscall_exit(-1);
     }
 }
 
 static void 
-syscall_halt (struct intr_frame* fr UNUSED) 
+syscall_halt (void) 
 {
   shutdown_power_off();
 }
 
 static void
-syscall_exit (struct intr_frame* fr UNUSED, int status)
+syscall_exit (int status)
 {
   thread_current()->exit_status = status;
   thread_exit();
 }
 
 static tid_t
-syscall_exec (struct intr_frame* fr, const char *cmd_line)
+syscall_exec (const char *cmd_line)
 {
-  if (!is_valid_string(fr->esp, cmd_line, false)) 
-    syscall_exit(fr, -1);
+  if (!is_valid_string(cmd_line, false)) 
+    syscall_exit(-1);
   return process_execute(cmd_line);
 }
 
 static int
-syscall_wait (struct intr_frame* fr UNUSED, tid_t pid)
+syscall_wait (tid_t pid)
 {
   return process_wait(pid);
 }
 
 static bool
-syscall_create (struct intr_frame* fr, const char *file, off_t initial_size)
+syscall_create (const char *file, off_t initial_size)
 {
-  if (!is_valid_string(fr->esp, file, false)) 
-    syscall_exit (fr, -1);
+  if (!is_valid_string(file, false)) 
+    syscall_exit (-1);
   lock_acquire (&fs_lock);
   bool success = filesys_create(file, initial_size);
   lock_release (&fs_lock);
@@ -168,10 +168,10 @@ syscall_create (struct intr_frame* fr, const char *file, off_t initial_size)
 }
 
 static bool
-syscall_remove (struct intr_frame* fr, const char *file)
+syscall_remove (const char *file)
 {
-  if (!is_valid_string(fr->esp, file, false)) 
-    syscall_exit (fr, -1);
+  if (!is_valid_string(file, false)) 
+    syscall_exit (-1);
   lock_acquire (&fs_lock);
   bool success = filesys_remove(file);
   lock_release (&fs_lock);
@@ -179,10 +179,10 @@ syscall_remove (struct intr_frame* fr, const char *file)
 }
 
 static int
-syscall_open (struct intr_frame* fr, const char *file)
+syscall_open (const char *file)
 {
-  if (!is_valid_string(fr->esp, file, false)) 
-    syscall_exit (fr, -1);
+  if (!is_valid_string(file, false)) 
+    syscall_exit (-1);
   lock_acquire (&fs_lock);
   struct file *f = filesys_open(file);
   lock_release (&fs_lock);
@@ -193,11 +193,11 @@ syscall_open (struct intr_frame* fr, const char *file)
 }
 
 static int
-syscall_filesize (struct intr_frame* fr UNUSED, int fd)
+syscall_filesize (int fd)
 {
   struct file *f = process_get_file(fd);
   if (f == NULL) {
-    syscall_exit(fr, -1);
+    syscall_exit(-1);
   }
   lock_acquire (&fs_lock);
   int size = file_length(f);
@@ -206,13 +206,13 @@ syscall_filesize (struct intr_frame* fr UNUSED, int fd)
 }
 
 static int
-syscall_read (struct intr_frame* fr, int fd, void *buffer, unsigned size)
+syscall_read (int fd, void *buffer, unsigned size)
 {
   if (size == 0) 
     return 0;
 
-  if (!is_valid_vrange(fr->esp, buffer, size, true)) 
-    syscall_exit(fr, -1);
+  if (!is_valid_vrange(buffer, size, true)) 
+    syscall_exit(-1);
 
   if (fd == STDIN_FILENO) 
     {      
@@ -225,7 +225,7 @@ syscall_read (struct intr_frame* fr, int fd, void *buffer, unsigned size)
 
   struct file *f = process_get_file(fd);
   if (f == NULL) 
-    syscall_exit(fr, -1);
+    syscall_exit(-1);
   
   lock_acquire (&fs_lock);
   int bytes_read = file_read(f, buffer, (off_t)size);
@@ -234,13 +234,13 @@ syscall_read (struct intr_frame* fr, int fd, void *buffer, unsigned size)
 }
 
 static int
-syscall_write (struct intr_frame* fr, int fd, const void *buffer, unsigned size)
+syscall_write (int fd, const void *buffer, unsigned size)
 {
   if (size == 0) 
     return 0;
 
-  if (!is_valid_vrange(fr->esp, buffer, size, false)) 
-    syscall_exit(fr, -1);
+  if (!is_valid_vrange(buffer, size, false)) 
+    syscall_exit(-1);
 
   if (fd == STDOUT_FILENO) 
     {
@@ -250,7 +250,7 @@ syscall_write (struct intr_frame* fr, int fd, const void *buffer, unsigned size)
 
   struct file *f = process_get_file(fd);
   if (f == NULL) 
-    syscall_exit(fr, -1);
+    syscall_exit(-1);
   
   lock_acquire (&fs_lock);
   int bytes_written = file_write(f, buffer, (off_t)size);
@@ -259,22 +259,22 @@ syscall_write (struct intr_frame* fr, int fd, const void *buffer, unsigned size)
 }
 
 static void
-syscall_seek (struct intr_frame* fr UNUSED, int fd, unsigned position)
+syscall_seek (int fd, unsigned position)
 {
   struct file *f = process_get_file(fd);
   if (f == NULL) 
-    syscall_exit(fr, -1);
+    syscall_exit(-1);
   lock_acquire (&fs_lock);
   file_seek(f, (off_t)position);
   lock_release (&fs_lock);
 }
 
 static unsigned
-syscall_tell (struct intr_frame* fr UNUSED, int fd)
+syscall_tell (int fd)
 {
   struct file *f = process_get_file(fd);
   if (f == NULL) 
-    syscall_exit(fr, -1);
+    syscall_exit(-1);
   lock_acquire (&fs_lock);
   unsigned position = file_tell(f);
   lock_release (&fs_lock);
@@ -282,11 +282,11 @@ syscall_tell (struct intr_frame* fr UNUSED, int fd)
 }
 
 static void
-syscall_close (struct intr_frame* fr UNUSED, int fd)
+syscall_close (int fd)
 {
   struct file *f = process_get_file(fd);
   if (f == NULL) 
-    syscall_exit(fr, -1);
+    syscall_exit(-1);
   process_close_file(fd);
 }
 
@@ -296,7 +296,7 @@ syscall_close (struct intr_frame* fr UNUSED, int fd)
    memory, and it is mapped to a page in the current
    process's page table. Returns false otherwise. */
 static bool 
-is_valid_vaddr (const void* esp UNUSED, const void *vaddr, bool write) 
+is_valid_vaddr (const void *vaddr, bool write) 
 {
 #ifdef VM
   if (vaddr == NULL || !is_user_vaddr(vaddr)) 
@@ -320,27 +320,27 @@ is_valid_vaddr (const void* esp UNUSED, const void *vaddr, bool write)
 /* Returns true if the given virtual address range is valid,
    false otherwise. */
 static bool 
-is_valid_vrange (const void* esp, const void *vaddr, unsigned size, bool write) 
+is_valid_vrange (const void *vaddr, unsigned size, bool write) 
 {
   const void *end = vaddr + size - 1;
   if (end < vaddr) {
     return false;
   }
-  return is_valid_vaddr(esp, vaddr, write) && is_valid_vaddr(esp, end, write);
+  return is_valid_vaddr(vaddr, write) && is_valid_vaddr(end, write);
 }
 
 static bool 
-is_valid_word (const void* esp, const void *vaddr, bool write)
+is_valid_word (const void *vaddr, bool write)
 {
-  return is_valid_vrange(esp, vaddr, sizeof(int), write);
+  return is_valid_vrange(vaddr, sizeof(int), write);
 }
 
 /* Returns true if the given string is valid, false 
    otherwise. */
 static bool
-is_valid_string (const void* esp, const char *str, bool write)
+is_valid_string (const char *str, bool write)
 {
-  while (is_valid_vaddr(esp, str, write)) {
+  while (is_valid_vaddr(str, write)) {
     if (*str == '\0') {
       return true;
     }
